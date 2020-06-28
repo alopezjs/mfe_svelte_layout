f51a197efa2a5592cb26f0ff0c3fdc59009d2a24
AndresLopez <andres_lopez@crgl-thirdparty.com>
2020-06-27 21:15:20 -0500

    testing

---
+++ src/spaEntry.js
@@ -0,0 +1,10 @@
+import singleSpaSvelte from "single-spa-svelte";
+import myRootSvelteComponent from "./main";
+const svelteLifecycles = singleSpaSvelte({
+  component: myRootSvelteComponent,
+  domElementGetter: () => document.getElementById("svelte-app"),
+  props: { someData: "data" },
+});
+export const bootstrap = svelteLifecycles.bootstrap;
+export const mount = svelteLifecycles.mount;
+export const unmount = svelteLifecycles.unmount;
